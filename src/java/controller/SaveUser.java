fab9008df38bf4f342048121d4d6893f46977982
SahanDilusha <sahancodelygroup@gmail.com>
2024-08-31 11:56:21 +0530

    create User bean

--- src/java/controller/SaveUser.java
+++ src/java/controller/SaveUser.java
@@ -7,12 +7,19 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 
 @WebServlet(name = "SaveUser", urlPatterns = {"/SaveUser"})
 public class SaveUser extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        SessionFactory  sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = sessionFactory.openSession();
+        
+        session.save(req);
         
     }
 
---
+++ src/java/model/User.java
@@ -0,0 +1,45 @@
+package model;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    private int id;
+    private String name;
+    private String mobile;
+
+    public User() {
+
+    }
+
+    public User(int id, String name, String mobile) {
+        this.id = id;
+        this.name = name;
+        this.mobile = mobile;
+    }
+
+}
